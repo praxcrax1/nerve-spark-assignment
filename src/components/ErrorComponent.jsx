bb47033a5e2f63d6a27dc109c455f6233d5fe8c4
Prakhar Gupta <prakharyologupta@gmail.com>
2024-04-28 20:06:23 +0530

    final

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,11 +1,17 @@
 // ErrorComponent.js
 import React from "react";
 import { useLoading } from "./LoadingContext";
+import "./ErrorComponent.css"; // Import CSS file for styling
 
 const ErrorComponent = () => {
   const { error } = useLoading();
 
-  return error ? <div>Error: {error}</div> : null;
+  return error ? (
+    <div className="error-box">
+      {/* Add a container with error styling */}
+      Error: {error}
+    </div>
+  ) : null;
 };
 
 export default ErrorComponent;
--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,11 +1,19 @@
 // LoadingSpinner.js
 import React from "react";
 import { useLoading } from "./LoadingContext";
+import "./LoadingSpinner.css"; // Import CSS file for styling
 
 const LoadingSpinner = () => {
   const { loading } = useLoading();
 
-  return loading ? <div>Loading...</div> : null;
+  return loading ? (
+    <div className="loading-spinner-overlay">
+      {" "}
+      {/* Add a container with fixed positioning */}
+      <div className="loading-spinner">Loading...</div>{" "}
+      {/* Add the loading spinner */}
+    </div>
+  ) : null;
 };
 
 export default LoadingSpinner;
